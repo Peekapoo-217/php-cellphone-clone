81a3b90920c5c1828a147b36c9bab0ebd2a3dc00
Peekapoo-217 <conghung2172004@gmail.com>
2025-02-01 16:41:31 +0700

    commited

--- connect.php
+++ connect.php
@@ -3,7 +3,7 @@
 // Thực hiện kết nối đến cơ sở dữ liệu
 $servername = "localhost"; // Thay đổi thành tên máy chủ MySQL của bạn
 $username = "root"; // Thay đổi thành tên người dùng MySQL của bạn
-$password = ""; // Thay đổi thành mật khẩu MySQL của bạn
+$password = "123456"; // Thay đổi thành mật khẩu MySQL của bạn
 $dbname = "CellPhone_K"; // Thay đổi thành tên cơ sở dữ liệu của bạn
 
 // Tạo kết nối
